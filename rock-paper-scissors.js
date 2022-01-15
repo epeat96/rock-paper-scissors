a8d02a65f374d09b6407fbd66d83cf72c644d01e
Epeat <44922559+epeat96@users.noreply.github.com>
2022-01-15 18:09:15 -0300

    feat: add functions for the computer to pick a 'weapon'

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,14 +5,11 @@ const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const pickStatus = document.getElementById('pickStatus');
+const opponentPick = document.getElementById('opponentPick');
 let pickOptions = [rock, paper, scissors];
+let gameStatus = 'prePick';
 
-const takePick = (e) => {
-    
-    let picked = e.target.id;
-    let notPicked = pickOptions.filter( (item) => item.id != picked );
-    notPicked.map( (item) => item.style.visibility = 'hidden' );
-    pickStatus.innerHTML = 'You picked '+picked+'!';
+const playSound = (picked) => {
 
     if(picked == 'rock'){
         rockSound.play();
@@ -20,6 +17,52 @@ const takePick = (e) => {
         paperSound.play();
     }else{
         scissorsSound.play();
+    }
+
+}
+
+const opponentPicker = () => {
+    let opponentPick = pickOptions[Math.floor(Math.random() * 3)].id;
+    return opponentPick; 
+}
+
+const showOpponentPick = (picked) => {
+
+    let img = document.createElement('img');
+    img.src='./images/'+picked+'.png';
+    opponentPick.appendChild(img);
+    playSound(picked);
+    gameStatus = 'end';
+
+}
+
+const takePick = (e) => {
+
+    if ( gameStatus == 'opponentPicking' ){
+        return;
+    }
+
+    if ( gameStatus == 'end' ){
+        pickOptions.map( (item) => item.style.visibility = 'visible' );
+        gameStatus = 'prePick';
+        pickStatus.innerHTML = 'Take your pick';
+        opponentPick.removeChild(opponentPick.firstElementChild);
+        return;
+    }
+
+    gameStatus = 'postPick';
+
+    let picked = e.target.id;
+    let notPicked = pickOptions.filter( (item) => item.id != picked );
+    notPicked.map( (item) => item.style.visibility = 'hidden' );
+    pickStatus.innerHTML = 'You picked '+picked+'!';
+
+    playSound(picked);
+
+    if ( gameStatus == 'postPick' ){
+        gameStatus = 'opponentPicking';
+        setTimeout(showOpponentPick,4000,opponentPicker());
+        return;
     }
 
 };
